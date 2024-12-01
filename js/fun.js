1e97ea6c79c3272dd553b7e68bc62022014476d1
edu-salcedo <edwin_15_89@hotmail.com>
2024-12-01 19:27:22 -0300

    agregar guiones dinamicamente

--- js/fun.js
+++ js/fun.js
@@ -1,1 +1,25 @@
+window.onload = init;
+const btn = document.getElementById("btn1");
+const card = document.getElementById("sugerencias");
+const listaUl = document.querySelectorAll(".ullist");
+const widthCard = card.clientWidth;
+function init() {
+
+    listaUl.forEach(lusi => {
+
+        const listaLi = lusi.getElementsByTagName("li");
+        console.log(listaLi);
+        // se usa metoda Array.prototype.forEach.call para trabajar con  htmlcollecccions
+        Array.prototype.forEach.call(listaLi, el => {
+            let hyphen = el.querySelector(".hyphen"); // creo variable par poder introducir guiones en espacios en blanco
+            const span = el.querySelector(".sp").offsetWidth; //creo constante span con el ancho en px del texto
+            while (hyphen.offsetWidth + span < widthCard - 50) { //mientras el ancho de guiones + el ancho del texto sean menores al ancho del card
+                hyphen.textContent += "-";    // se agrega un guion mas al texto        
+            }
+        });
+    });
+}
+
+
+
 
